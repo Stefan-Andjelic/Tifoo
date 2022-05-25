9ceee3512e8b45c2249e7577f53e9e09853d2548
Stefan <stefanandjelic8@gmail.com>
2022-05-25 00:38:45 -0500

    added product list page with category filtering

--- Tifoo/urls.py
+++ Tifoo/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
+    path('', include('shop.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
